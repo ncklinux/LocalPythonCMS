77f095b24abb7a3a591bfe9167ce4cd4b7aff173
Nick <68464552+ncklinux@users.noreply.github.com>
2022-11-07 15:02:06 +0100

    feat: add database class

---
+++ database.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+
+class Database(object):
+
+    __DB_LOCATION = "localpythoncms.sqlite"
+
+    def __init__(self):
+        self.__db_connection = sqlite3.connect(self.__DB_LOCATION)
+        self.cur = self.__db_connection.cursor()
+        self.cur.execute(
+            """CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            firstname varchar(50) NOT NULL,
+            lastname varchar(50) NOT NULL,
+            email varchar(100) NOT NULL,
+            username varchar(50) NOT NULL,
+            password varchar(128) NOT NULL)"""
+        )
+        self.cur.execute(
+            "INSERT INTO users VALUES (null, ?, ?, ?, ?, ?)",
+            (
+                "test_firstname",
+                "test_lastname",
+                "test@localpythoncms.local",
+                "test",
+                "123User.",
+            ),
+        )
+        self.__db_connection.commit()
+
+    def __del__(self):
+        self.__db_connection.close()
