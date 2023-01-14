b393aec941d4d7e943cdcbcdac5a5a8e22d8ccf7
Nick <68464552+ncklinux@users.noreply.github.com>
2023-01-14 20:11:50 +0100

    refactor: change var names

--- common/logger_factory.py
+++ common/logger_factory.py
@@ -4,22 +4,22 @@ import logging
 class LoggerFactory(object):
     @staticmethod
     def createLogger(name):
-        file_formatter = logging.Formatter(
+        fileFormatter = logging.Formatter(
             "%(asctime)s~%(levelname)s~%(message)s~module:%(module)s~function:%(module)s"
         )
-        console_formatter = logging.Formatter("%(levelname)s -- %(message)s")
+        consoleFormatter = logging.Formatter("%(levelname)s -- %(message)s")
 
-        file_handler = logging.FileHandler("logs/logdata.log")
-        file_handler.setLevel(logging.WARN)
-        file_handler.setFormatter(file_formatter)
+        fileHandler = logging.FileHandler("logs/logdata.log")
+        fileHandler.setLevel(logging.WARN)
+        fileHandler.setFormatter(fileFormatter)
 
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.DEBUG)
-        console_handler.setFormatter(console_formatter)
+        consoleHandler = logging.StreamHandler()
+        consoleHandler.setLevel(logging.DEBUG)
+        consoleHandler.setFormatter(consoleFormatter)
 
         logger = logging.getLogger(name)
-        logger.addHandler(file_handler)
-        logger.addHandler(console_handler)
+        logger.addHandler(fileHandler)
+        logger.addHandler(consoleHandler)
         logger.setLevel(logging.DEBUG)
 
         return logger
