37d86c72d3c23a52719a122374923d440f79e855
Nick <68464552+ncklinux@users.noreply.github.com>
2023-01-14 20:06:25 +0100

    feat: add logger factory class

---
+++ common/logger_factory.py
@@ -0,0 +1,25 @@
+import logging
+
+
+class LoggerFactory(object):
+    @staticmethod
+    def createLogger(name):
+        file_formatter = logging.Formatter(
+            "%(asctime)s~%(levelname)s~%(message)s~module:%(module)s~function:%(module)s"
+        )
+        console_formatter = logging.Formatter("%(levelname)s -- %(message)s")
+
+        file_handler = logging.FileHandler("logs/logdata.log")
+        file_handler.setLevel(logging.WARN)
+        file_handler.setFormatter(file_formatter)
+
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+        console_handler.setFormatter(console_formatter)
+
+        logger = logging.getLogger(name)
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+        logger.setLevel(logging.DEBUG)
+
+        return logger
